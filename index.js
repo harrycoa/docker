b29a6818aa7bda5fb1d6e9b5cab82c6685453abf
harry coa <rodol_2000@hotmail.com>
2022-08-29 13:17:29 -0500

    feat: monitorear archivos para docker

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     if (err) {
       res.status(500).send('💥 harry 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me conecté a la DB2 ....! 😎');
       db.close();
     }
   });
